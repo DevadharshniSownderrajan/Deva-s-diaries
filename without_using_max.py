99010295ba62706b23c80a668822f068741c85c6
Devadharshni_Sownderrajan <devasdiary01@gmail.com>
2025-12-10 20:59:19 +0530

    Create without_using_max.py

---
+++ without_using_max.py
@@ -0,0 +1,16 @@
+a = 10
+b = 15
+c = 89
+d = 7
+e = 34
+
+max_num = a
+if b > max_num:
+    max_num = b
+if c > max_num:
+    max_num = c
+if d > max_num:
+    max_num = d
+if e > max_num:
+    max_num=e
+print(max_num)
