9c7f6447bc0349ef3308b87e8c4c071369d5e0fc
Devadharshni_Sownderrajan <devasdiary01@gmail.com>
2026-01-11 22:12:10 +0530

    Create eligibility.py

---
+++ eligibility.py
@@ -0,0 +1,11 @@
+percentage = int(input("The percentage"))
+if percentage > 70:
+    name = input("Enter ur name:")
+    department = input("Enter your department:")
+    location = input("Enter your location:")
+    print("name:",name)
+    print("department:",department)
+    print("location:",location)
+    print("You are eligible")
+else:
+    print("You are not eligible")
