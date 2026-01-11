c61ecf3432ea4389ee13d294791f0517b3b4a011
Devadharshni_Sownderrajan <devasdiary01@gmail.com>
2026-01-11 22:11:10 +0530

    Create mini_calculator.py

---
+++ mini_calculator.py
@@ -0,0 +1,11 @@
+a = int(input("A:"))
+b = int(input("B:"))
+operation = input("add/sub/mul/div")
+if (operation == "add"):
+    print("The addition of the number is",a+b)
+elif (operation == "sub"):
+    print("The subtraction of the number is",a-b)
+elif (operation == "mul"):
+    print("The multiplication of the number is",a*b)
+elif (operation == "div"):
+    print("The division of the number is:",a/b)
